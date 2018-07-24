37907513cf229ee0bb98587114e4ed0b6531647c
lexuanchinh <1510305@hcmut.edu.vn>
2018-07-24 11:04:07 +0700

    Profile

--- frontend/baron/app/src/main/java/gggroup/com/baron/main/profile/ProfileFragment.kt
+++ frontend/baron/app/src/main/java/gggroup/com/baron/main/profile/ProfileFragment.kt
@@ -1,19 +1,105 @@
 package gggroup.com.baron.main.profile
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.widget.LinearLayout
+import android.widget.TextView
 import gggroup.com.baron.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(),AppBarLayout.OnOffsetChangedListener {
 
+
+    private val PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f
+    private val PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f
+    private val ALPHA_ANIMATIONS_DURATION = 200
+
+    private var mIsTheTitleVisible = false
+    private var mIsTheTitleContainerVisible = true
+
+    private var mTitleContainer: LinearLayout? = null
+    private var mTitle: TextView? = null
+    private var mAppBarLayout: AppBarLayout? = null
+    private var mToolbar: Toolbar? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, null)
+       var view=inflater.inflate(R.layout.fragment_profile, container,false)
+    //    bindActivity()
+//        ic_dog.setImageDrawable(
+//                ContextCompat.getDrawable(
+//                        applicationContext, // Context
+//                        R.drawable.ic_dog// Drawable
+//                ))
+
+        main_appbar.addOnOffsetChangedListener(this)
+
+        startAlphaAnimation(this.mTitle!!, 0, View.INVISIBLE)
+        return view
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+    private fun bindActivity() {
+        mToolbar = view!!.findViewById(R.id.main_toolbar) as Toolbar
+        mTitle = view!!.findViewById(R.id.main_textview_title) as TextView
+        mTitleContainer = view!!.findViewById(R.id.main_linearlayout_title) as LinearLayout
+        mAppBarLayout = view!!.findViewById(R.id.main_appbar) as AppBarLayout
+    }
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, offset: Int) {
+        val maxScroll = appBarLayout.totalScrollRange
+        val percentage = Math.abs(offset).toFloat() / maxScroll.toFloat()
+
+        handleAlphaOnTitle(percentage)
+        handleToolbarTitleVisibility(percentage)
+    }
+    private fun handleAlphaOnTitle(percentage: Float) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if (mIsTheTitleContainerVisible) {
+                startAlphaAnimation(this.main_linearlayout_title!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
+                mIsTheTitleContainerVisible = false
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(this.main_linearlayout_title!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
+                mIsTheTitleContainerVisible = true
+            }
+        }
+    }
+
+    private fun handleToolbarTitleVisibility(percentage: Float) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if (!mIsTheTitleVisible) {
+                startAlphaAnimation(this.main_textview_title!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
+                mIsTheTitleVisible = true
+            }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(this.main_textview_title!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
+                mIsTheTitleVisible = false
+            }
+        }
+    }
+
+
+    fun startAlphaAnimation(v: View, duration: Long, visibility: Int) {
+        val alphaAnimation = if (visibility == View.VISIBLE)
+            AlphaAnimation(0f, 1f)
+        else
+            AlphaAnimation(1f, 0f)
+
+        alphaAnimation.duration = duration
+        alphaAnimation.fillAfter = true
+        v.startAnimation(alphaAnimation)
     }
 }
