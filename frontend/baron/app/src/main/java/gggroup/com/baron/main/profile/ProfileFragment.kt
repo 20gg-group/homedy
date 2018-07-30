ffda1700ee7c0cccdf68b9b749f7347194130e76
lexuanchinh <1510305@hcmut.edu.vn>
2018-07-30 17:27:51 +0700

    update profile

--- frontend/baron/app/src/main/java/gggroup/com/baron/main/profile/ProfileFragment.kt
+++ frontend/baron/app/src/main/java/gggroup/com/baron/main/profile/ProfileFragment.kt
@@ -11,90 +11,17 @@ import android.view.animation.AlphaAnimation
 import android.widget.LinearLayout
 import android.widget.TextView
 import gggroup.com.baron.R
-class ProfileFragment : Fragment(),AppBarLayout.OnOffsetChangedListener {
+class ProfileFragment : Fragment() {
 
 
-    private val PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f
-    private val PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f
-    private val ALPHA_ANIMATIONS_DURATION = 200
 
-    private var mIsTheTitleVisible = false
-    private var mIsTheTitleContainerVisible = true
-
-    private var mTitleContainer: LinearLayout? = null
-    private var mTitle: TextView? = null
-    private var mAppBarLayout: AppBarLayout? = null
-    private var mToolbar: Toolbar? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
        var view=inflater.inflate(R.layout.fragment_profile, container,false)
-        mToolbar = view!!.findViewById(R.id.main_toolbar) as Toolbar
-        mTitle = view.findViewById(R.id.main_textview_title) as TextView
-        mTitleContainer = view.findViewById(R.id.main_linearlayout_title) as LinearLayout
-        mAppBarLayout = view.findViewById(R.id.main_appbar) as AppBarLayout
-    //    bindActivity()
-//        ic_dog.setImageDrawable(
-//                ContextCompat.getDrawable(
-//                        applicationContext, // Context
-//                        R.drawable.ic_dog// Drawable
-//                ))
-        mAppBarLayout!!.addOnOffsetChangedListener(this)
-
-        startAlphaAnimation(this.mTitle!!, 0, View.INVISIBLE)
         return view
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    override fun onOffsetChanged(appBarLayout: AppBarLayout, offset: Int) {
-        val maxScroll = appBarLayout.totalScrollRange
-        val percentage = Math.abs(offset).toFloat() / maxScroll.toFloat()
 
-        handleAlphaOnTitle(percentage)
-        handleToolbarTitleVisibility(percentage)
-    }
-    private fun handleAlphaOnTitle(percentage: Float) {
-        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
-            if (mIsTheTitleContainerVisible) {
-                startAlphaAnimation(this.mTitleContainer!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
-                mIsTheTitleContainerVisible = false
-            }
-
-        } else {
-
-            if (!mIsTheTitleContainerVisible) {
-                startAlphaAnimation(this.mTitleContainer!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
-                mIsTheTitleContainerVisible = true
-            }
-        }
-    }
-
-    private fun handleToolbarTitleVisibility(percentage: Float) {
-        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
-
-            if (!mIsTheTitleVisible) {
-                startAlphaAnimation(this.mTitle!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
-                mIsTheTitleVisible = true
-            }
-
-        } else {
-
-            if (mIsTheTitleVisible) {
-                startAlphaAnimation(this.mTitle!!, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
-                mIsTheTitleVisible = false
-            }
-        }
-    }
-
-
-    fun startAlphaAnimation(v: View, duration: Long, visibility: Int) {
-        val alphaAnimation = if (visibility == View.VISIBLE)
-            AlphaAnimation(0f, 1f)
-        else
-            AlphaAnimation(1f, 0f)
-
-        alphaAnimation.duration = duration
-        alphaAnimation.fillAfter = true
-        v.startAnimation(alphaAnimation)
-    }
 }
